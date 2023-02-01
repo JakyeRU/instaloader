fbbb3f4da6f3665e07029c9ce4ade124f09b48e5
Jakye <jakyeru@gmail.com>
2023-02-01 09:58:38 +0200

    Exiting the process if the profile is up to date.

--- instaloaderManager.py
+++ instaloaderManager.py
@@ -15,7 +15,8 @@ def download_posts(L, profile):
         if L.download_post(post, target=profile.username):
             terminal.info(f'Post "{post.caption}" has been downloaded. [{index+1}/{profile.mediacount}]')
         else:
-            terminal.error(f'Post "{post.caption}" couldn\'t be downloaded. [{index+1}/{profile.mediacount}]')
+            terminal.info(f'This profile is up to date.')
+            exit(0)
 
     terminal.success(f'{"Updated" if os.path.exists(f"./{profile.username}") else "Downloaded"} profile {profile.username}.')
 
